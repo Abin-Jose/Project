0d8e6534db50a6b6ba234032719f4cc18a0e390e
abin <abin@ndimensionz.com>
2017-10-30 19:15:53 +0530

    change in api

--- src/app/service/common.service.ts
+++ src/app/service/common.service.ts
@@ -12,18 +12,18 @@ export class CommonService {
   }
 
   getMovies(page){
-    return this.http.get('https://api.themoviedb.org/3/discover/movie?api_key=2a99205beaea6ff4733a680d1fe92346&language=en-US&primary_release_year=2017&page='+page).map(res=>res.json());
+    return this.http.get('https://api.themoviedb.org/3/discover/movie?api_key=&language=en-US&primary_release_year=2017&page='+page).map(res=>res.json());
   }
 
   getConfig(){
-    return this.http.get('https://api.themoviedb.org/3/configuration?api_key=2a99205beaea6ff4733a680d1fe92346').map(res=>res.json());
+    return this.http.get('https://api.themoviedb.org/3/configuration?api_key=').map(res=>res.json());
   }
 
   getGenre(){
-     return this.http.get('https://api.themoviedb.org/3/genre/movie/list?api_key=2a99205beaea6ff4733a680d1fe92346&language=en-US').map(res=>res.json());
+     return this.http.get('https://api.themoviedb.org/3/genre/movie/list?api_key=&language=en-US').map(res=>res.json());
   }
 
   getTvshows(){
-    return this.http.get('https://api.themoviedb.org/3/discover/tv?api_key=2a99205beaea6ff4733a680d1fe92346&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false').map(res=>res.json());
+    return this.http.get('https://api.themoviedb.org/3/discover/tv?api_key=&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false').map(res=>res.json());
   }
 }
